7ee2085f4966f8acc57f2f1b6bedfa04937523f5
VineethR147 <vineeth.181ee254@nitk.edu.in>
2019-10-16 03:24:23 +0530

    2nd

--- _init_.py
+++ _init_.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template,flash
+from flask import Flask, render_template,flash,request
 from forms import LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] ='1'
 
-@app.route("/")
+
 @app.route("/login",methods=['GET','POST'])
 def login():
 	form= LoginForm()
 	if form.validate_on_submit():
+		if request.cookies.get('admin') == "true":
+			flash(f'flag{cookies_are_fun}')
 		flash(f'Successfullylogged in as  {form.username.data}!','success')
 	return render_template('login.html',form=form)
 
